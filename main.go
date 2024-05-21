0f7e8ed976bb78901411613a6a70780fa9b19a5e
David Matijanić <dmatijani21@foi.hr>
2024-05-21 10:48:53 +0200

    Make simple REST service

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"go-server/api"
+	"net/http"
+)
 
 func main() {
-	http.HandleFunc("/hello-world", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world"))
-	})
-
-	http.ListenAndServe(":8080", nil)
+	srv := api.NewServer()
+	http.ListenAndServe(":8080", srv)
 }
